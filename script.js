f12b1d9bdf7a2e1375d910b94f7099ecf4a42ddc
MSreeN <khyathimnsree369@gmail.com>
2023-11-18 22:55:57 +0530

    testing with promises on parcel

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 // import { addToCart } from "./ShoppingCart.js";
-import 'core-js'
+import "core-js";
 console.log("importing shopping cart");
 
 // addToCart("shirt")
 
-import addToCart,* as ShoppingCart from './ShoppingCart.js'
-import cloneDeep from './node_modules/lodash-es'
-
-const {shoppingCost} = ShoppingCart;
+import addToCart, * as ShoppingCart from "./ShoppingCart.js";
 
 // addToCart('shirt')
 // console.log('cost is', shoppingCost);
@@ -17,12 +14,10 @@ const createSupplier = (function () {
   const field = "automobile";
   const price = 500;
 
-
   const getSupplierName = () => {
     console.log(price);
     return name;
   };
-
 
   return {
     name,
@@ -31,8 +26,6 @@ const createSupplier = (function () {
   };
 })();
 
-
-
 createSupplier.name;
 createSupplier.field;
 console.log(createSupplier.getName());
@@ -40,30 +33,37 @@ console.log(createSupplier.getName());
 //this gives an error because price is private to that iife module and we can't access it outside, this is module pattern. look up on internet for more information
 // console.log(createSupplier.price);
 
-
 //below code is used to maintain state of project using parcel, fo example you have an array of 3 objects inside it, on first render it will show array with 3 objects in it, if you save it then render it again then array would have 6 objects and on next render array will have 12 objects
 //what is happening is parcel is saving previous state and adding new state to the previous state
 //findings: objects are getting doubled after refreshing the application/page, this feature is meant to be used in development, we don't refresh the application every time when application is in development
-if(module.hot){
-  module.hot.accept(function(){
-    ShoppingCart.cart.splice(0, ShoppingCart.cart.length)
+if (module.hot) {
+  module.hot.accept(function () {
+    ShoppingCart.cart.splice(0, ShoppingCart.cart.length);
   });
 }
-addToCart('apple')
+addToCart("apple");
 console.log(ShoppingCart.cart);
 
 let name = "sree";
 console.log(name);
 
-class Person{
+class Person {
   #name;
-  constructor(name){
+  constructor(name) {
     this.#name = name;
   }
 }
 
-const sree = new Person("sree")
+const sree = new Person("sree");
 console.log(sree);
 
-const a = ShoppingCart.cart.find(ele => ele == "apple")
+const a = ShoppingCart.cart.find((ele) => ele == "apple");
 console.log(a);
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("promise resolved");
+  }, 2000);
+});
+
+console.log(promise);
